229f8121fff9d5d717f56a0138be4d8f8c8cf9ef
Jaid616 <78507633+Jaid616@users.noreply.github.com>
2023-01-19 14:11:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const route = require("./routes/route")
 PORT =  process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(corsOptions))
-
+app.use(function(req,res,next)=>{
+        res.header("Access-Control-Access-Origin","*");
+         res.header("Access-Control-Access-Credentials",true);
+         res.header("Access-Control-Access-Methods","GET , PUT , POST , DELETE , OPTIONS");
+          res.header("Access-Control-Access-Headers","Origin , X-Requested-with,Content-Type,Accept,content-type,application/json");
+          next();
+        
+        })
 app.use("/", route);
 
 const start = async()=>{
