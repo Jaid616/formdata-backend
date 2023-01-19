f6c676f0e04109f881146de6a52c0bc73fa27f38
Jaid616 <78507633+Jaid616@users.noreply.github.com>
2023-01-19 14:21:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 
 const express = require('express');
 const Connect = require("./config/Connect")
-const cors = require("cors")
-const corsOptions = require("./config/corsOptions")
 const app = express();
 const route = require("./routes/route")
 PORT =  process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors(corsOptions))
-
+app.use(function(req,res,next) {
+        res.header("Access-Control-Allow-Origin","*");
+         res.header("Access-Control-Allow-Credentials",true);
+         res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE,OPTIONS");
+          res.header("Access-Control-Allow-Headers","Origin , X-Requested-with,Content-Type,Accept,content-type,application/json");
+          next();
+        
+        });
 app.use("/", route);
 
 const start = async()=>{
